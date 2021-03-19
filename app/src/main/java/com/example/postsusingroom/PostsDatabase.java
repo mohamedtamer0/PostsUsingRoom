b0cc074cb04a3ebc2253f530ef33aacc16a25d05
mohamed tamer0 <mt4452242@gmail.com>
2021-03-19 19:10:21 +0200

    update

---
+++ app/src/main/java/com/example/postsusingroom/PostsDatabase.java
@@ -0,0 +1,30 @@
+package com.example.postsusingroom;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Post.class}, version = 2)
+public abstract class PostsDatabase extends RoomDatabase {
+
+    private static PostsDatabase instance;
+
+    public abstract PostsDao postsDao();
+
+
+    public static synchronized PostsDatabase getInstance(Context context) {
+
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    PostsDatabase.class, "posts_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
+
+
+}
